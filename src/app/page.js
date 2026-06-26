71c1ab88a539363358b07c4fa297b9706d50e3da
nhankun2006 <luongthiennhana2@gmail.com>
2026-06-26 21:47:24 +0700

    Sửa tab1 và làm mới tab3

--- src/app/page.js
+++ src/app/page.js
@@ -380,7 +380,7 @@ export default function Home() {
             <div className="w-full aspect-video bg-black rounded-2xl overflow-hidden border-2 border-[#ff99c4]/30 shadow-[0_0_30px_rgba(255,153,196,0.2)] relative">
               <iframe 
                 className="absolute top-0 left-0 w-full h-full"
-                src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=0" 
+                src="https://www.youtube.com/embed/8sVtL0o-v7U" 
                 title="YouTube video player" 
                 frameBorder="0" 
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
